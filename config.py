8217726efc813d4d05837057617ad40b6c517334
Botmaster55 <wajsarif461@gmail.com>
2026-06-30 21:42:00 +0530

    Change default values for subscription channels
    Updated default values for FORCE_SUB_CHANNEL2, FORCE_SUB_CHANNEL3, and FORCE_SUB_CHANNEL4.

--- config.py
+++ config.py
@@ -16,9 +16,9 @@ from logging.handlers import RotatingFileHandler
 
 # Specify the Telegram channel or group ID that users must join (force-subscribe) before using the bot.
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002500863343"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", ""))
-FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", ""))
-FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", ""))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002500863343"))
+FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1002500863343"))
+FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "-1002500863343"))
 
 
 # Display the Bot's Uptime
